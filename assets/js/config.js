09cf5fa7f04d42db2fcadac88a97df5221f1cd9e
Shaluka Gimhan <shalukagimhan13@gmail.com>
2026-05-07 13:47:55 +0530

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     // Backend API URL for Socket Connection (Pairing & QR)
-    BACKEND_URL: "https://uptight-ketti-chamimusic-3cce977f.koyeb.app",
+    BACKEND_URL: "https://meaningful-eliza-chamimusic-afc92c75.koyeb.app",
     
     // You can add more global web configurations here in the future
 };
